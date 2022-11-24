08829d154407cdf75592e55444d159b7eff5f88c
dnsmoly <dn.smoly@gmail.com>
2022-11-24 23:27:36 +0300

    Library version 2.0 initial commit

--- src/main/java/com/smoly/controllers/PeopleController.java
+++ src/main/java/com/smoly/controllers/PeopleController.java
@@ -1,12 +1,12 @@
 package com.smoly.controllers;
 
-import com.smoly.dao.BookDAO;
+import com.smoly.services.BooksService;
+import com.smoly.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import com.smoly.dao.PersonDAO;
 import com.smoly.models.Person;
 
 import javax.validation.Valid;
@@ -16,25 +16,25 @@ import javax.validation.Valid;
 @RequestMapping("/people")
 public class PeopleController {
 
-    private final PersonDAO personDAO;
-    private final BookDAO bookDAO;
+    private final PeopleService peopleService;
+
 
     @Autowired
-    public PeopleController(PersonDAO personDAO, BookDAO bookDAO) {
-        this.personDAO = personDAO;
-        this.bookDAO = bookDAO;
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     @GetMapping()
     public String index(Model model) {
-        model.addAttribute("people", personDAO.index());
+        model.addAttribute("people", peopleService.findAll());
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
-        model.addAttribute("books", bookDAO.getBooksByOwnerId(id));
+
+        model.addAttribute("person", peopleService.findOne(id));
+        model.addAttribute("books", peopleService.getBooksByPersonId(id));
 
         return "people/show";
     }
@@ -50,13 +50,13 @@ public class PeopleController {
         if (bindingResult.hasErrors())
             return "people/new";
 
-        personDAO.save(person);
+        peopleService.save(person);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("person", personDAO.show(id));
+        model.addAttribute("person", peopleService.findOne(id));
         return "people/edit";
     }
 
@@ -66,13 +66,13 @@ public class PeopleController {
         if (bindingResult.hasErrors())
             return "people/edit";
 
-        personDAO.update(id, person);
+        peopleService.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
-        personDAO.delete(id);
+        peopleService.delete(id);
         return "redirect:/people";
     }
 }
--- src/main/java/com/smoly/models/Person.java
+++ src/main/java/com/smoly/models/Person.java
@@ -1,28 +1,39 @@
 package com.smoly.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.util.List;
 
+@Entity
+@Table(name = "person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "name")
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 50, message = "Name should be between 2 and 50")
     @Pattern(regexp = "(?U)[A-ZА-Я]\\w+ [A-ZА-Я]\\w+",
              message = "Name should be in form of \"Name Lastname\"")
     private String name;
 
+    @Column(name = "year_of_birth")
     @Min(value = 1900, message = "Year of birth should be greater than 1900")
-    private int year;
+    private int yearOfBirth;
 
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
 
     public Person() {
 
     }
 
-    public Person(int id, String name, int year) {
-        this.id = id;
+    public Person(String name, int yearOfBirth) {
         this.name = name;
-        this.year = year;
+        this.yearOfBirth = yearOfBirth;
     }
 
     public int getId() {
@@ -41,11 +52,28 @@ public class Person {
         this.name = name;
     }
 
-    public int getYear() {
-        return year;
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
     }
 }
